b37298be7bbbbab267bfbc01c58cb227c00c8376
charlieholy <357483226@qq.com>
2018-04-11 16:32:11 +0800

    add qr-

--- ex/exDocServer.js
+++ ex/exDocServer.js
@@ -2,14 +2,28 @@
 var express = require('express');
 var app = express();
 var htmldoc = require('../markdown/markdown')
+
+var qr_code = require('qr-encode')
+var qr_image = require('qr-image');
+
+
 app.get('/doc/rainbow', function (req, res) {
-    res.send(htmldoc.htmldoc);
+    var _doc = htmldoc.getDoc('rainbow.md');
+    _doc += "<img src='http://localhost/doc/qr_code'/>"
+    res.send(_doc);
 })
 
 app.get('/doc/bitcoin-cli', function (req, res) {
     var _doc = htmldoc.getDoc('bitcoin-cli.md')
     res.send(_doc);
 })
+
+app.get('/doc/qr_code', function (req, res) {
+    var temp_qrcode = qr_image.image('goodluck!');
+    res.type('png');
+    temp_qrcode.pipe(res);
+})
+
 
 var server = app.listen(80, function () {
 
